7d1acdabbefc03cc2c8e72d12591557f2039d374
unknown <pr1@BANL07EE48A8A.corp.intuit.net>
2015-04-18 15:37:20 +0530

    added javadoc

--- src/main/java/com/springpeople/App.java
+++ src/main/java/com/springpeople/App.java
@@ -6,6 +6,7 @@ package com.springpeople;
  */
 public class App 
 {
+	/** Hello this is the Main Method */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!...GitHub" );
